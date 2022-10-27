3dee2653005b88a246d4375982f2b760efa66c35
briidee3 <briidee3@gmail.com>
2022-10-27 11:00:09 -0400

    set up basic viewport functionality

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
 window.onload = () => {
     
+    let slides = document.getElementsByClassName(`slides`);
+    let slideWidth = 500;
+
+    console.log(slideWidth);
+    // position slides
+    for (let i = 0; i < slides.length; i++) {
+        console.log(slides[i].style.left = `${i * slideWidth}px`);
+    }
+
     
 };
